86bedfa4cab5f96923a0bef7c5deb7a614fd49fa
Viet Doan <vietdht@yahoo.com.vn>
2024-05-30 15:21:49 +0700

    add redirect config

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  redirects: async () => {
+    return [
+      {
+        source: '/signin',
+        destination: 'http://localhost:3555/api/auth/discord',
+        permanent: false,
+        basePath: false
+      },
+    ];
+  },
   images: {
     remotePatterns: [
       {
