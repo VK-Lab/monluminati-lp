448b33106adde4a24e225027adb6035330a14fa1
kiennguyen <djhahe@gmail.com>
2024-06-05 22:32:33 +0700

    remove unuse file

--- src/gql/gql.ts
+++
@@ -1,9 +0,0 @@
-/* eslint-disable */
-import { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-node/core';
-
-export function graphql(source: string): unknown;
-export function graphql(source: string) {
-  return (documents as any)[source] ?? {};
-}
-
-export type DocumentType<TDocumentNode extends DocumentNode<any, any>> = TDocumentNode extends DocumentNode<  infer TType,  any>  ? TType  : never;
